ad3072f4dbe5f875f6d0006e6274ab03307be51b
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2023-05-29 16:28:30 +0300

    [ttcodegen2]: Remove all files

--- src/utils/writeError.ts
+++
@@ -1,8 +0,0 @@
-import chalk from "chalk";
-
-export const writeError = (text: string, code?: number) =>
-  console.error(
-    chalk.bgRed("ERROR"),
-    chalk.redBright(text),
-    code && `${chalk.gray("code:")} ${chalk.whiteBright(code)}`
-  );
